0a78d201f6583cbfc46a92b1fc4d43352d3d39df
digthedill <popssly@gmail.com>
2021-07-29 18:17:46 -0500

    banner links to product page

--- components/HeroHead.js
+++ components/HeroHead.js
@@ -1,20 +1,23 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 const HeroHead = () => {
   return (
-    <div className="relative min-w-screen flex justify-center items-center cursor-pointer h-60">
-      <Image
-        alt="bg-tyedye"
-        src="/swirls.jpg"
-        layout="fill"
-        objectFit="cover"
-        quality={100}
-        className="z-0 opacity-60"
-      />
-      <h1 className="text-5xl sm:text-6xl md:text-8xl tracking-wide z-10">
-        EMPTY THREATS
-      </h1>
-    </div>
+    <Link href="/products">
+      <div className="relative min-w-screen flex justify-center items-center cursor-pointer h-60">
+        <Image
+          alt="bg-tyedye"
+          src="/swirls.jpg"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+          className="z-0 opacity-60"
+        />
+        <h1 className="text-5xl sm:text-6xl md:text-8xl tracking-wide z-10">
+          EMPTY THREATS
+        </h1>
+      </div>
+    </Link>
   )
 }
 
